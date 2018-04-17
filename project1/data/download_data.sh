bcbcd9301236a9acacae2a1f083b1aef9e46d558
Ondrej Skopek <skopekondrej@gmail.com>
2018-04-17 22:46:25 +0200

    Added test dataset download

--- project1/data/download_data.sh
+++ project1/data/download_data.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -x
 wget https://polybox.ethz.ch/index.php/s/qUc2NvUh2eONfEB/download -O data.tar
 tar -xvf data.tar
-wget https://polybox.ethz.ch/index.php/s/cpicEJeC2G4tq9U/download -O pretrained_embeddings
+wget https://polybox.ethz.ch/index.php/s/cpicEJeC2G4tq9U/download -O pretrained_embedding
+wget https://polybox.ethz.ch/index.php/s/HJUnOuIj3K4FEdT/download -O sentences.test
