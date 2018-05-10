8686d72469d5bf1b81a6c0ccf61e5614b8b5d282
Ondrej Skopek <skopekondrej@gmail.com>
2018-05-10 12:10:54 +0200

    Minor typos

--- project2/data/download_data.sh
+++ project2/data/download_data.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
-wget https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download -O stories.train
-wget https://polybox.ethz.ch/index.php/s/huqymt5WErXxpuW/download -O stories.eval
+wget https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download -O stories.train.csv
+wget https://polybox.ethz.ch/index.php/s/huqymt5WErXxpuW/download -O stories.eval.csv
