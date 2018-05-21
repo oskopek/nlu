b7a4799e82b92a98ca5aee3f9ffb2053b8fb938d
Ondrej Skopek <skopekondrej@gmail.com>
2018-05-21 18:40:36 +0200

    Fixed model to new data

--- project2/sct/model/model.py
+++ project2/sct/model/model.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from datetime import datetime
 import os
-from typing import *
+from typing import Tuple, Dict, Union, List, Any
 
 from sct.data.datasets import Datasets
 from sct.data.stories import StoriesDataset
@@ -21,14 +21,14 @@ class Model:
         self.global_step = tf.Variable(0, dtype=tf.int64, trainable=False, name="global_step")
 
         # [batch_size, SENTENCES x sentence_id]
-        self.sentence_ids = tf.placeholder(tf.int32, [None, self.TOTAL_SENTENCES], name="sentence_ids")
+        self.batch_to_sentences = tf.placeholder(tf.int32, [None, self.TOTAL_SENTENCES], name="batch_to_sentences")
 
         # [unique sentence_ids, max_word_ids]
-        self.word_ids = tf.placeholder(tf.int32, [None, None], name="word_ids")
+        self.sentence_to_words = tf.placeholder(tf.int32, [None, None], name="sentence_to_words")
         self.sentence_lens = tf.placeholder(tf.int32, [None], name="sentence_lens")
 
         # [unique word_ids, max_char_ids]
-        self.char_ids = tf.placeholder(tf.int32, [None, None], name="char_ids")
+        self.word_to_chars = tf.placeholder(tf.int32, [None, None], name="word_to_chars")
         self.word_lens = tf.placeholder(tf.int32, [None], name="word_lens")
 
         # [batch_size]
@@ -91,10 +91,14 @@ class Model:
     def _build_feed_dict(self, batch: DotMap[str, Union[np.ndarray, bool]],
                          is_training: bool = False) -> Dict[tf.Tensor, Union[np.ndarray, bool]]:
         assert is_training == batch.is_training
-        return {self.sentence_ids: batch.sentence_ids,
-                self.word_ids: batch.word_ids, self.sentence_lens: batch.sentence_lens,
-                self.char_ids: batch.char_ids, self.word_lens: batch.word_lens,
-                self.labels: batch.labels, self.is_training: batch.is_training
+        return {
+                self.batch_to_sentences: batch.batch_to_sentences,
+                self.sentence_to_words: batch.sentence_to_words,
+                self.sentence_lens: batch.sentence_lens,
+                self.word_to_chars: batch.word_to_chars,
+                self.word_lens: batch.word_lens,
+                self.labels: batch.labels,
+                self.is_training: batch.is_training
         }
 
     @staticmethod
@@ -115,12 +119,12 @@ class Model:
 
         def _eval_metrics() -> Dict[str, str]:
             dataset = "eval"
-            eval_metrics = self.evaluate_epoch(data.eval_data, dataset, batch_size=batch_size)
+            eval_metrics = self.evaluate_epoch(data.eval, dataset, batch_size=batch_size)
             return self._tqdm_metrics(dataset, eval_metrics, ["acc", "loss"])
 
         epoch_tqdm = tqdm.tqdm(range(epochs), desc="Epochs")
         for epoch in epoch_tqdm:
-            batch_count, batch_generator = data.batches_per_epoch(batch_size, data=data.train_data)
+            batch_count, batch_generator = data.train.batches_per_epoch(batch_size)
             batch_tqdm = tqdm.tqdm(range(batch_count), desc=f"Batches [Epoch {epoch}]")
             for _ in batch_tqdm:
                 batch = next(batch_generator)
@@ -130,7 +134,7 @@ class Model:
 
     def evaluate_epoch(self, data: StoriesDataset, dataset: str, batch_size: int = 1) -> List[float]:
         self.session.run(self.reset_metrics)
-        for batch in data.batches_per_epoch_generator(batch_size, data=data):
+        for batch in data.batch_per_epoch_generator(batch_size, shuffle=False):
             self.session.run(self.update_metrics, self._build_feed_dict(batch))
         metrics, _ = self.session.run(self.current_metrics + [self.summaries[dataset]])
         return metrics
@@ -139,7 +143,7 @@ class Model:
         self.session.run(self.reset_metrics)
         predictions = []
         self.session.run(self.reset_metrics)
-        for batch in data.batches_per_epoch_generator(batch_size, data=data):
+        for batch in data.batch_per_epoch_generator(batch_size, shuffle=False):
             batch_predictions = self.session.run(self.predictions, self._build_feed_dict(batch))
             predictions.extend(batch_predictions)
         self.session.run(self.summaries[dataset])
--- project2/sct/model/rnn.py
+++ project2/sct/model/rnn.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import Tuple
 
 import tensorflow as tf
 
@@ -18,6 +18,7 @@ class RNN(Model):
                  char_embedding: int = 100,
                  keep_prob: float = 0.5,
                  learning_rate: float = 1e-4,
+                 grad_clip: float = 10.0,
                  **kwargs) -> None:
         super(RNN, self).__init__(*args, **kwargs)
         self.rnn_cell = rnn_cell
@@ -28,6 +29,7 @@ class RNN(Model):
         self.char_embedding = char_embedding
         self.word_embedding = word_embedding
         self.learning_rate = learning_rate
+        self.grad_clip = grad_clip
 
     def _create_cell(self) -> tf.nn.rnn_cell.LayerRNNCell:
         if self.rnn_cell == "LSTM":
@@ -39,51 +41,46 @@ class RNN(Model):
 
     def _char_embeddings(self) -> tf.Tensor:
         if self.char_embedding == -1:
-            input_chars = tf.one_hot(self.char_ids, self.num_chars)
+            input_chars = tf.one_hot(self.word_to_chars, depth=self.num_chars)
         else:
             char_emb_mat = tf.get_variable("char_emb", shape=[self.num_chars, self.char_embedding])
-            input_chars = tf.nn.embedding_lookup(char_emb_mat, self.char_ids)
+            input_chars = tf.nn.embedding_lookup(char_emb_mat, self.word_to_chars)
         print("input_chars", input_chars.get_shape())
-        return input_chars
 
-    def _char_rnn(self, input_chars: tf.Tensor) -> tf.Tensor:
         rnn_cell_characters = self._create_cell()
         _, (state_fw, state_bw) = tf.nn.bidirectional_dynamic_rnn(
                 cell_fw=rnn_cell_characters,
                 cell_bw=rnn_cell_characters,
                 inputs=input_chars,
-                sequence_length=self.charseq_lens,
+                sequence_length=self.word_lens,
                 dtype=tf.float32,
                 scope="rnn_chars")
         input_chars = tf.concat([state_fw, state_bw], axis=1)
         print("input_chars_rnn", input_chars.get_shape())
 
-        charseq_ids = tf.concat([self.sentence_charseq_ids, self.ending_charseq_ids], axis=1)
-        print("charseq_ids", charseq_ids.get_shape())
-        shape = charseq_ids.get_shape()
-        charseq_ids_flat = tf.reshape(charseq_ids, tf.stack([shape[0] * shape[1], shape[2], shape[3]]))
-        input_char_words = tf.nn.embedding_lookup(input_chars, charseq_ids_flat)
-        input_char_words = tf.layers.dropout(input_char_words, rate=self.keep_prob, training=self.is_training)
+        input_char_words = tf.nn.embedding_lookup(input_chars, self.sentence_to_words)
         print("input_char_words", input_char_words.get_shape())
         return input_char_words
 
     def _word_embeddings(self) -> tf.Tensor:
-        word_ids = tf.concat([self.sentence_word_ids, self.ending_word_ids], axis=1)
-        print("word_ids", word_ids.get_shape())
-        shape = word_ids.get_shape()
-        word_ids_flat = tf.reshape(word_ids, tf.stack([shape[0] * shape[1], shape[2], shape[3]]))
+        # [batch_size, SENTENCE x sentence_id, max_word_ids]
+        batch_to_sentences = tf.nn.embedding_lookup(self.sentence_to_words, ids=self.batch_to_sentences)
+        shape = batch_to_sentences.get_shape()
+        print("batch_to_sentences", shape)
+        sentence_to_words = tf.reshape(batch_to_sentences, tf.stack([shape[0] * shape[1], shape[2], shape[3]]))
 
         if self.word_embedding == -1:
-            input_words = tf.one_hot(word_ids_flat, self.num_words)
+            sentence_word_embeddings = tf.one_hot(sentence_to_words, self.num_words)
         else:
             word_emb_mat = tf.get_variable("word_emb", shape=[self.num_words, self.word_embedding])
-            input_words = tf.nn.embedding_lookup(word_emb_mat, word_ids_flat)
-            input_words = tf.layers.dropout(input_words, rate=self.keep_prob, training=self.is_training)
-        print("input_words", input_words.get_shape())
-        return input_words
+            sentence_word_embeddings = tf.nn.embedding_lookup(word_emb_mat, sentence_to_words)
+            sentence_word_embeddings = tf.layers.dropout(
+                    sentence_word_embeddings, rate=self.keep_prob, training=self.is_training)
+        print("sentence_word_embeddings", sentence_word_embeddings.get_shape())
+        return sentence_word_embeddings
 
-    def _word_rnn(self, inputs: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
-        lens = tf.concat([self.sentence_lens, self.ending_lens], axis=1)
+    def _sentence_rnn(self, inputs: tf.Tensor) -> tf.Tensor:
+        lens = tf.nn.embedding_lookup(self.sentence_lens, self.batch_to_sentences)
         lens_flat = tf.reshape(lens, (-1))
 
         rnn_cell_words = self._create_cell()
@@ -93,31 +90,35 @@ class RNN(Model):
                 inputs=inputs,
                 sequence_length=lens_flat,
                 dtype=tf.float32)
-        states = tf.concat([state_fw, state_bw], axis=1)
-        print("states", states.get_shape())
-        states_unflat = tf.reshape(states, (-1, self.SENTENCES, self.ENDINGS, self.rnn_cell_dim * 2))
+        sentence_wordword_states = tf.concat([state_fw, state_bw], axis=1)
+        print("sentence_wordword_states", sentence_wordword_states.get_shape())
+        return sentence_wordword_states
+
+    def _story_embeddings(self, sentence_wordword_states: tf.Tensor) -> Tuple[tf.Tensor, Tuple[tf.Tensor, tf.Tensor]]:
+        states_unflat = tf.reshape(sentence_wordword_states, (-1, self.SENTENCES + self.ENDINGS, self.rnn_cell_dim * 2))
         print("states_unflat", states_unflat.get_shape())
         states_sentences = states_unflat[:, :self.SENTENCES, :]
         states_endings = states_unflat[:, -self.ENDINGS:, :]
         print("states separate", states_sentences.get_shape(), states_endings.get_shape())
-        return states_sentences, states_endings
 
-    def _fc(self, states_sentences: tf.Tensor, states_endings: tf.Tensor) -> tf.Tensor:
+        sentence_state = tf.layers.flatten(states_sentences)
+        ending1_state = tf.squeeze(states_endings[:, 0, :], axis=1)
+        ending2_state = tf.squeeze(states_endings[:, 1, :], axis=1)
+        ending1_state = tf.layers.flatten(ending1_state)
+        ending2_state = tf.layers.flatten(ending2_state)
+        return sentence_state, (ending1_state, ending2_state)
+
+    def _fc(self, sentence_state: tf.Tensor, ending_states: Tuple[tf.Tensor, tf.Tensor]) -> tf.Tensor:
         with tf.variable_scope("sentences_fc"):
-            sentences_flat = tf.layers.flatten(states_sentences)
-            sentences_fc = tf.layers.dense(sentences_flat, 1024, activation=tf.nn.leaky_relu)
+            sentences_fc = tf.layers.dense(sentence_state, 1024, activation=tf.nn.leaky_relu)
             sentences_fc = tf.layers.dense(sentences_fc, 512, activation=tf.nn.leaky_relu)
             print("sentences_fc", sentences_fc.get_shape())
         with tf.variable_scope("endings_fc"):
-            ending = tf.squeeze(states_endings[:, 0, :], axis=1)
-            ending_flat = tf.layers.flatten(ending)
-            ending_fc = tf.layers.dense(ending_flat, 1024, activation=tf.nn.leaky_relu, name="fc1")
+            ending_fc = tf.layers.dense(ending_states[0], 1024, activation=tf.nn.leaky_relu, name="fc1")
             ending1_output = tf.layers.dense(ending_fc, 512, activation=tf.nn.leaky_relu, name="fc2")
             print("ending1_output", ending1_output.get_shape())
-        with tf.variable_scope("endings_fc"):
-            ending = tf.squeeze(states_endings[:, 1, :], axis=1)
-            ending_flat = tf.layers.flatten(ending)
-            ending_fc = tf.layers.dense(ending_flat, 1024, activation=tf.nn.leaky_relu, name="fc1")
+        with tf.variable_scope("endings_fc", reuse=tf.AUTO_REUSE):
+            ending_fc = tf.layers.dense(ending_states[1], 1024, activation=tf.nn.leaky_relu, name="fc1")
             ending2_output = tf.layers.dense(ending_fc, 512, activation=tf.nn.leaky_relu, name="fc2")
             print("ending2_output", ending2_output.get_shape())
         with tf.variable_scope("common_fc"):
@@ -131,20 +132,18 @@ class RNN(Model):
     def build_model(self) -> Tuple[tf.Tensor, tf.Tensor, tf.Operation]:
         # Construct the graph
         with self.session.graph.as_default():
-            with tf.name_scope("char_embeddings"):
-                input_chars = self._char_embeddings()
-
-            with tf.name_scope("char_rnn"):
-                input_char_words = self._char_rnn(input_chars)
+            with tf.name_scope("charword_embeddings"):
+                sentence_charword_states = self._char_embeddings()
 
             with tf.name_scope("word_embeddings"):
-                input_words = self._word_embeddings()
+                sentence_word_embeddings = self._word_embeddings()
 
-            inputs = tf.concat([input_char_words, input_words], axis=2)
-            print("inputs", inputs.get_shape())
+            with tf.name_scope("sentence_embeddings"):
+                inputs = tf.concat([sentence_charword_states, sentence_word_embeddings], axis=2)
+                sentence_wordword_states = self._sentence_rnn(inputs)
 
-            with tf.name_scope("word_rnn"):
-                states_sentences, states_endings = self._word_rnn(inputs)
+            with tf.name_scope("story_embeddings"):
+                states_sentences, states_endings = self._story_embeddings(sentence_wordword_states)
 
             with tf.name_scope("fc"):
                 output_layer = self._fc(states_sentences, states_endings)
@@ -158,7 +157,7 @@ class RNN(Model):
             with tf.name_scope("optimizer"):
                 optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
                 gradients = optimizer.compute_gradients(self.loss)
-                clipped_gradients = [(tf.clip_by_norm(gradient, 5), var) for gradient, var in gradients]
+                clipped_gradients = [(tf.clip_by_norm(gradient, self.grad_clip), var) for gradient, var in gradients]
                 training_step = optimizer.apply_gradients(clipped_gradients, global_step=self.global_step)
 
         return predictions, loss, training_step
