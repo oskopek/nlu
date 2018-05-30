d8ff67376edbf1418830a7381cbaa3a53dbc2ace
Lukas Jendele <jendelel@ethz.ch>
2018-05-30 10:24:13 +0200

    Fixed link to the eval dataset

--- project2/data/download_data.sh
+++ project2/data/download_data.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 wget https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download -O stories.train.csv
-wget https://polybox.ethz.ch/index.php/s/huqymt5WErXxpuW/download -O stories.eval.csv
+wget https://polybox.ethz.ch/index.php/s/02IVLdBAgVcsJAx/download -O stories.eval.csv
